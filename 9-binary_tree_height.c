1c7fbc41dd52974df2de442e069b6e35ade79d7a
Sohila hamdy <sohila.fathy22@gmail.com>
2024-08-16 05:53:29 -0700

    Initial commit - Add binary tree node function and test

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -22,3 +22,26 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	else
 		return (right_height + 1);
 }
+
+/**
+ * binary_tree_height_edges - Measures the height of a binary tree by edges
+ *
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: The height of the tree in terms of edges, or 0 if tree is NULL
+ */
+size_t binary_tree_height_edges(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+
+	left_height = binary_tree_height_edges(tree->left);
+	right_height = binary_tree_height_edges(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
+}
