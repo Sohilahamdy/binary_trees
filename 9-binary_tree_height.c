76f687a3cbba6a7820c9b56c163df4032df05e05
Sohila hamdy <sohila.fathy22@gmail.com>
2024-08-16 05:57:10 -0700

    Initial commit - Add binary tree node function and test

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,39 +9,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
-	if (tree == NULL)
-		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
-}
-
-/**
- * binary_tree_height_edges - Measures the height of a binary tree by edges
- *
- * @tree: Pointer to the root node of the tree to measure the height
- *
- * Return: The height of the tree in terms of edges, or 0 if tree is NULL
- */
-size_t binary_tree_height_edges(const binary_tree_t *tree)
-{
-	size_t left_height, right_height;
-
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-		return (0);
-
-	left_height = binary_tree_height_edges(tree->left);
-	right_height = binary_tree_height_edges(tree->right);
-
-	if (left_height > right_height)
-		return (left_height + 1);
-	else
-		return (right_height + 1);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
